4fc2cacb604defc62b514e208057ce059fbaa5ca
David Mazieres <dm@garage.uun.org>
2010-09-14 01:26:17 -0700

    fix Search.hs

--- Data/IterIO/Search.hs
+++ Data/IterIO/Search.hs
@@ -2,6 +2,7 @@
 module Data.IterIO.Search (inumStopString
                           ) where
 
+import Control.Monad.Trans
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy.Search as Search
@@ -9,41 +10,45 @@ import qualified Data.ByteString.Lazy.Search as Search
 import Data.IterIO
 
 -- | Feeds input to an Iteratee until some boundary string is found.
--- The boundary string is then discarded.
 inumStopString :: (Monad m) =>
                   L8.ByteString
                -> Inum L8.ByteString L8.ByteString m a
-inumStopString pat0 = mkInum $ nextChunk L8.empty
+inumStopString pat0 = mkInumM $ nextChunk L8.empty
     where
       spat = Search.strictify pat0
       lpat = L8.fromChunks [spat]
       plen = toEnum $ S8.length spat
       search = Search.breakOn spat
       nextChunk old = do
-        (Chunk t eof) <- chunkI
+        (Chunk t eof) <- lift chunkI
         case search $ L8.append old t of
-          (a, b) | not (L8.null b) -> Done (CodecE a) (chunk $ L8.drop plen b)
-          (a, _) | eof             -> return (CodecE a)
+          (a, b) | not (L8.null b) -> do lift $ Done () (chunk $ L8.drop plen b)
+                                         ifeed a
+          (a, _) | eof             -> ifeed a
           (a, _)                   -> checkEnd a
       checkEnd t = let tlen = L8.length t
-                       hlen = max 0 $ (tlen - plen - 1)
+                       hlen = max 0 (tlen - plen - 1)
                        ttail = L8.drop hlen t
                        fpm = firstPossibleMatch 0 ttail
                        rlen = hlen + fpm
                    in if rlen == tlen
-                      then return $ CodecF (nextChunk L8.empty) t
+                      then ifeed t >> nextChunk L8.empty
                       else case L8.splitAt rlen t of
-                             (r, o) -> return (nextChunk o) r
+                             (r, o) -> ifeed r >> nextChunk o
       firstPossibleMatch n t =
           if t `L8.isPrefixOf` lpat
           then n
           else firstPossibleMatch (n + 1) (L8.tail t)
 
-
 {-
 main :: IO ()
 main = enumHandle stdin |$ do
-         inumStopString "TheEnd" .| handleI stdout
+         inumStopString (L8.pack "TheEnd") .| out
          liftIO $ putStrLn "\n\n*** We have reached THE END #1 ***\n\n"
-         inumStopString "TheEnd" .| handleI stdout
+         inumStopString (L8.pack "TheEnd") .| out
+         liftIO $ putStrLn "\n\n*** We have reached THE END #2 ***\n\n"
+         out
+    where
+      out = catchOrI (dataI >>= liftIO . L8.putStr)
+            (\(IterEOF _) -> return ()) (const out)
 -}
