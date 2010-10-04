35f20ccf5f2dfa3310c013a437a9c2c93014f8c3
David Mazieres <dm@vivisex.scs.stanford.edu>
2010-10-04 11:13:53 +0200

    be more aggressive about closing, but httpsEcho still needs content-length

--- Data/IterIO/SSL.hs
+++ Data/IterIO/SSL.hs
@@ -2,6 +2,7 @@ module Data.IterIO.SSL where
 
 import qualified Data.ByteString.Lazy as L
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
+import Control.Concurrent
 import Control.Exception (throwIO, ErrorCall(..))
 import Control.Monad
 import Control.Monad.Trans
@@ -10,7 +11,8 @@ import qualified OpenSSL.Session as SSL
 import System.Cmd
 import System.Exit
 
-import Data.IterIO
+import Data.IterIO.Base
+import Data.IterIO.Inum
 
 -- | Simple OpenSSL 'Onum'.
 enumSsl :: (MonadIO m) => SSL.SSL -> Onum L.ByteString m a
@@ -18,12 +20,13 @@ enumSsl ssl = mkInumM $ irepeat $
               liftIO (SSL.read ssl defaultChunkSize) >>=
                      ifeed1 . L.fromChunks . (: [])
 
--- | Simple OpenSSL 'Iter'.
+-- | Simple OpenSSL 'Iter'.  Does not do any kind of shutdown.
 sslI :: (MonadIO m) => SSL.SSL -> Iter L.ByteString m ()
-sslI ssl = do
-  Chunk t eof <- chunkI
-  unless (L.null t) $ liftIO $ SSL.lazyWrite ssl t
-  when eof $ liftIO $ SSL.shutdown ssl SSL.Unidirectional
+sslI ssl = loop
+    where loop = do
+            Chunk t eof <- chunkI
+            unless (L.null t) $ liftIO $ SSL.lazyWrite ssl t
+            if eof then return () else loop
 
 -- | Turn a socket into an 'Iter' and 'Onum' that use OpenSSL to read
 -- and write from the socket, respectively.  Don't forget to call this
@@ -37,9 +40,20 @@ sslFromSocket :: (MonadIO m) =>
               -- ^ 'True' for server handshake, 'False' for client
               -> IO (Iter L.ByteString m (), Onum L.ByteString m a)
 sslFromSocket ctx sock server = do
+  mn <- newMVar 2
   ssl <- SSL.connection ctx sock
   if server then SSL.accept ssl else SSL.connect ssl
-  return (sslI ssl, enumSsl ssl)
+  let cleanW = liftIO $ modifyMVar mn $ \n -> do
+               if (n < 2)
+                 then do SSL.shutdown ssl SSL.Bidirectional
+                         Net.sClose sock
+                 else SSL.shutdown ssl SSL.Unidirectional
+               return (n - 1, ())
+      cleanR = mkInumM $ liftIO $ modifyMVar mn $ \n -> do
+               when (n < 2) $ do SSL.shutdown ssl SSL.Bidirectional
+                                 Net.sClose sock
+               return (n - 1, ())
+  return (sslI ssl >> cleanW, enumSsl ssl `cat` cleanR)
 
 -- | Simplest possible SSL context, loads cert and unencrypted private
 -- key from a single file.
