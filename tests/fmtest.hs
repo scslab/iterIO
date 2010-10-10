113a76cb4d3048e62b1f8c3b24db6d715794764f
David Mazieres <dm@garage.uun.org>
2010-10-09 18:34:46 -0700

    minor things I don't want to lose

--- tests/fmtest.hs
+++ tests/fmtest.hs
@@ -1,8 +1,16 @@
 
 module Main where
 
+import Control.Exception
+import Data.Monoid
+
 import Data.IterIO.Base
 import Data.IterIO.Extra
+
+-- | This shouldn't mappend to EOF, but did when there was a bug in feedI
+mappendEOF :: Iter () IO ()
+mappendEOF = feedI (IterF $ IterFail (toException $ ErrorCall "error"))
+             (Chunk mempty True)
 
 fmtest :: IO ()
 fmtest = run $ feedI testiter (chunk ())
