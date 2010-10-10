def57aa15006d9606f4227f3991c37f6ba750b40
David Mazieres <dm@garage.uun.org>
2010-10-09 19:12:09 -0700

    better test

--- tests/fmtest.hs
+++ tests/fmtest.hs
@@ -6,11 +6,19 @@ import Data.Monoid
 
 import Data.IterIO.Base
 import Data.IterIO.Extra
+import Data.IterIO.Parse
+
+{-
+-- | This shouldn't mappend to EOF, but did when there was a bug in feedI
+eof1 :: Iter () IO ()
+eof1 = feedI (IterF $ IterFail (toException $ ErrorCall "error"))
+       (Chunk mempty True)
+-}
+
 
 -- | This shouldn't mappend to EOF, but did when there was a bug in feedI
-mappendEOF :: Iter () IO ()
-mappendEOF = feedI (IterF $ IterFail (toException $ ErrorCall "error"))
-             (Chunk mempty True)
+eoftst :: Iter () IO ()
+eoftst = enumPure "ab" |$ skipMany (string "a" >> string "b")
 
 fmtest :: IO ()
 fmtest = run $ feedI testiter (chunk ())
